9ffdc5597789db96f64e46a98d8da88e6a960a45
Neumann Arne Michael <ArneMichael.Neumann@swisslife.de>
2021-04-07 15:47:30 +0200

    need to install alpine.patch, too

--- install.sh
+++ install.sh
@@ -7,4 +7,5 @@ PREFIX=${PREFIX:-"${HOME}/.local/bin"}
 
 install -m 755 host/copy_template_project.sh ${PREFIX}/copy_template_project.sh
 install -m 755 host/run_rmtoo.sh ${PREFIX}/run_rmtoo.sh
+install -m 644 host/alpine.patch ${PREFIX}/alpine.patch
 
